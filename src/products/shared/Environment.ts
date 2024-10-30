1b7af07d025262c66dabea226e722fe2a05532b2
JD93Lopez <juandavid.jdlea@gmail.com>
2024-10-30 10:03:36 -0500

    Nuevas ips back y front

--- src/products/shared/Environment.ts
+++ src/products/shared/Environment.ts
@@ -1,6 +1,6 @@
 export default class Environment {
   public static readonly getDomain = (): string => {
-    const HOST = process.env["HOST"] ?? 'ALB-PUBLIC-21587761.us-east-1.elb.amazonaws.com'
+    const HOST = process.env["HOST"] ?? 'LoadBFront-137448909.us-east-1.elb.amazonaws.com'
     const PROTOCOL = process.env["PROTOCOL"] ?? 'http'
     return `${PROTOCOL}://${HOST}`
   }
